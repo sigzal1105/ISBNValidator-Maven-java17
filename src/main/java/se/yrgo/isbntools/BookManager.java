5cdbc55e23f84d4ea1efc599056a87897816ac06
sigzal1105 <sigzal1105@skola.goteborg.se>
2024-02-09 18:56:20 +0100

    plzzz

---
+++ src/main/java/se/yrgo/isbntools/BookManager.java
@@ -0,0 +1,25 @@
+package se.yrgo.isbntools;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class BookManager {
+
+	public static void main(String[] args) {
+		List<Book> books = new ArrayList<Book>();
+		books.add(new Book("069117654X", "Understanding the Digital World","Brian Kernighan"));
+		books.add(new Book("0596009658","Learning the bash Shell","Cameron Newham"));
+		for(Book book:books) {
+			System.out.println(book);
+		}
+	}
+}
+
+
+
+
+
+
